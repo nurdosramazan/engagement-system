c630fff83d80b8c34d0cb2c7589e059b495611b3
nurdosramazan <nurdos.ramazan@gmail.com>
2025-09-21 00:16:18 -0400

    new spring boot app with base pom.xml

---
+++ src/main/java/com/epam/engagement_system/EngagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.epam.engagement_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/engagement_system/EngagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.engagement_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
