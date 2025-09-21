83c7aa14098a8a5c4e8e7a695a971b6251e2b517
nurdosramazan <nurdos.ramazan@gmail.com>
2025-09-21 17:53:51 -0400

    spring web connected - admin, appointment, auth and user controllers added

--- src/main/java/com/epam/engagement_system/controller/AppointmentController.java
+++ src/main/java/com/epam/engagement_system/controller/AppointmentController.java
@@ -1,4 +1,14 @@
 package com.epam.engagement_system.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/appointments")
 public class AppointmentController {
+    @GetMapping
+    public String test() {
+        return "Successful appointment endpoint!";
+    }
 }
--- src/main/java/com/epam/engagement_system/controller/UserController.java
+++ src/main/java/com/epam/engagement_system/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.epam.engagement_system.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+    @GetMapping
+    public String test() {
+        return "Successful user endpoint!";
+    }
 }
